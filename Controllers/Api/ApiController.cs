c40db96bf0b5a803db965568cfdf0f473701aefc
IkhsanHadi <ikhsanhdn747@gmail.com>
2025-04-17 13:32:46 +0700

    update

--- Controllers/Api/ApiController.cs
+++ Controllers/Api/ApiController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 [ApiController]
@@ -12,22 +13,25 @@ public class ApiController : ControllerBase {
     }
 
     [HttpGet("{sub_domain}/api/device/store")]
-    public async Task<IActionResult> StoreReading(string sub_domain) {
-        try {
-            var queryParams = HttpContext.Request.QueryString.Value;
-            var targetUrl = $"http://{sub_domain}.higertech.com/api/device/store{queryParams}";
+    public async Task<IActionResult> StoreReading(string sub_domain)
+    {
+        try
+        {
+            // Ambil semua query string key-value
+            var queryParams = HttpContext.Request.Query;
+            var queryString = string.Join("&", queryParams.Select(kvp => $"{kvp.Key}={Uri.EscapeDataString(kvp.Value)}"));
 
+            var targetUrl = $"http://{sub_domain}.higertech.com/api/device/store?{queryString}";
+
+            // Kirim request GET ke subdomain target
             var response = await _httpClient.GetAsync(targetUrl);
             var content = await response.Content.ReadAsStringAsync();
 
-            // return new ContentResult {
-            //     Content = content,
-            //     ContentType = "application/json",
-            //     StatusCode = (int)response.StatusCode
-            // };
-
-            return Ok("berhasil bypass");
-        } catch (Exception ex) {
+            // Kembalikan response dari server tujuan langsung sebagai JSON
+            return Content(content, "application/json", Encoding.UTF8);
+        }
+        catch (Exception ex)
+        {
             return StatusCode(500, new { error = ex.Message });
         }
     }
